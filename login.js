b6c9c7210cc522b7a389f209347f0a08c222c4d1
bhabesh.bharali <jsarma058@gmail.com>
2022-11-29 23:48:50 +0530

    login_reg

---
+++ login.js
@@ -0,0 +1,14 @@
+let userName="Bhabesh.Bharali";
+let passWord="@chikamika@"
+
+let logUser= userName;
+log logPass= passWord;
+
+if(logUser==userName && logPass==passWord)
+{
+console.log("Hi Baby You can log in");
+}
+else
+{
+console.log("just get lost");
+}
